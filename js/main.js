7bf8885c59910d950e9af060cd2261762d2c9748
nyatigo <sundaytigo@gmail.com>
2018-07-02 13:15:51 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,38 +1,33 @@
-var models = [
+//function fun(){
+  //  var search = document.getElementById("input1").value
+    //console.log(search)
 
-{
-  name:"GT350R",
-  image:"img/3.jpg",
-  price:"$64,740",
-  info:"The GT350 Track Attack is a complimentary high-performance driving experience for Shelby® GT350/GT350R owners. This one-day event is designed to build the high-performance driving skills that will help you get the most out of your new GT350/GT350R. For more information, just go to www.gt350trackattack.com or call 435-27-SPEED to register."
-  
-},
+   // $.ajax({
+     //   url:"https://www.googleapis.com/books/v1/volumes?q=" + search,
+       // dataType:'json',
+        //success: function(data){
+          //  for(var i = 0;i < data.items.length; i++){
+            //    document.body.innerHTML +="<p>" + data.items[i].volumeInfo.title + "</p>"
+            //}
+        //},
+        //type:'GET'
+    //})
 
-{
-  name:"GT",
-  image:"img/5.jpg",
-  price:"$35,190",
-  info:"Move like a legend.The 2017 Ford Mustang is a high performance, sport coupe that competes with the Chevrolet Camaro and Dodge Challenger. Traditionally, the Mustang has been an enduring symbol for muscle car enthusiasts. The Shelby GT350 will be covered in a separate overview. "
-  
-}
 
-];
+//}
 
-function fun(){
-  var input = document.getElementById("input").value
-  var h1 = document.getElementById("name")
-  var image = document.getElementById("img")
-  var h5 = document.getElementById("price")
-  var p = document.getElementById("para")
-  for(var i = 0 ; i < models.length ; i++){
-    if(input == models[i].name){
-      h1.innerHTML = models[i].name
-      image.src= models[i].image
-      h5.innerHTML = models[i].price
-      p.innerHTML = models[i].info
-      m.innerHTML = models[i].m
+//document.getElementById("btn").addEventListener("click",fun)
 
-    }
+
+$("button").click(function(){
+
+var request = $.ajax("https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse")
+
+request.done(
+  function(data){
+    console.log(data)
+
   }
+  )
 
-}
+})
